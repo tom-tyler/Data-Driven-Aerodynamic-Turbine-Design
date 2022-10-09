ce849040cd7a6113e743fa6f6b461aac0ae4a54c
Tom Tyler <tom-tyler@outlook.com>
2022-10-09 20:54:42 +0100

    started 1D

--- 1D_fitting.py
+++ 1D_fitting.py
@@ -1,1 +1,17 @@
+"""goal is to create a ML model for a quadratic"""
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.linspace(start=-10, stop=10, num=1000).reshape(-1, 1)
+y = np.squeeze(X**2 + 1)
+
+training_indices = np.random.randint(low=len(y),size=10)
+X_train, y_train = X[training_indices], y[training_indices]
+
+plt.plot(X, y, label=r"$f(x) = x^{2}$", linestyle="dotted")
+plt.scatter(X_train,y_train, label=r"$Samples$", marker="x")
+plt.legend()
+plt.xlabel("$x$")
+plt.ylabel("$f(x)$")
+plt.show()
