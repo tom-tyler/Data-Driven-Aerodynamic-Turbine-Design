98061d72c12756c98ec45fe95923c635ac8c82e8
Tom Tyler <tom-tyler@outlook.com>
2022-10-13 17:21:54 +0100

    trying to fill between two lines in 2D

--- 2D_fitting.py
+++ 2D_fitting.py
@@ -4,8 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
+from matplotlib.colors import ListedColormap
+from scipy.interpolate import UnivariateSpline
 
-grid_size=50
+grid_size=1000
 x = np.linspace(start=-10, stop=10, num=grid_size)
 y = np.linspace(start=-10, stop=10, num=grid_size)
 # print(np.dstack((x,y)))
@@ -16,7 +18,7 @@ P = np.column_stack((X_vector,Y_vector))
 Q = P[:,0]**2 - P[:,1]**2
 Z = X**2 - Y**2
 
-training_indices = np.random.randint(low=len(Q),size=30)
+training_indices = np.random.randint(low=len(Q),size=20)
 P_train, Q_train = P[training_indices], Q[training_indices]
 
 kernel = 1.0 * RBF(length_scale=(1.0,1.0))
@@ -24,19 +26,54 @@ gaussian_process = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=
 gaussian_process.fit(P_train, Q_train)
 print(gaussian_process.kernel_)
 
-testing_indices = np.random.randint(low=len(Q),size=30)
+testing_indices = np.random.randint(low=len(Q),size=20)
 P_train = P[testing_indices]
 
 mean_prediction, std_prediction = gaussian_process.predict(P, return_std=True)
 upper_confidence_interval = mean_prediction - 1.96 * std_prediction
 lower_confidence_interval = mean_prediction + 1.96 * std_prediction
 mean_prediction_grid = mean_prediction.reshape(grid_size,grid_size)
+std_prediction_grid = std_prediction.reshape(grid_size,grid_size)
+upper_confidence_interval_grid = upper_confidence_interval.reshape(grid_size,grid_size)
+lower_confidence_interval_grid = lower_confidence_interval.reshape(grid_size,grid_size)
 
+contour_levels = np.linspace(-80,80,5)
+#if row upper_grid
 
 fig, ax = plt.subplots()
-actual_plot = ax.contour(X, Y, Z, colors='red')
-predicted_plot = ax.contour(X, Y, mean_prediction_grid, colors='blue')
+actual_plot = ax.contour(X, Y, Z, colors='red',levels=contour_levels)
+predicted_plot = ax.contour(X, Y, mean_prediction_grid,colors='blue',levels=contour_levels)
+confidence_plot_upper = ax.contour(X, Y, upper_confidence_interval_grid,levels=contour_levels,colors='green')
+confidence_plot_lower = ax.contour(X, Y, lower_confidence_interval_grid,levels=contour_levels,colors='green')
 
+
+for contour_level in range(len(contour_levels)):
+    
+    contour_lines_upper = confidence_plot_upper.allsegs[contour_level]
+    contour_lines_lower = confidence_plot_lower.allsegs[contour_level]
+    
+    for contour_line in range(len(contour_lines_upper)):
+        
+        upper_contour = contour_lines_upper[contour_line][:,1]
+        old_indices = np.arange(0,len(upper_contour))
+        new_indices = np.linspace(0,len(upper_contour)-1,grid_size)
+        spl = UnivariateSpline(old_indices,upper_contour,k=3,s=0)
+        upper_contour = spl(new_indices)
+        
+        lower_contour = contour_lines_lower[contour_line][:,1]
+        old_indices = np.arange(0,len(lower_contour))
+        new_indices = np.linspace(0,len(lower_contour)-1,grid_size)
+        spl = UnivariateSpline(old_indices,lower_contour,k=3,s=0)
+        lower_contour = spl(new_indices)
+        
+        plt.fill_between(x=x,
+                        y1=lower_contour,
+                        y2=upper_contour,
+                        alpha=0.5,
+                        color='green')
+
+# ax.scatter(P_train[:,0],P_train[:,1],marker='x')
+# (zzmax <= 1) & (zzmin >= 1)
 h1,_ = actual_plot.legend_elements()
 h2,_ = predicted_plot.legend_elements()
 ax.legend([h1[0], h2[0]], [r"$f(x,y) = x^{2}-y^{2}$", "Mean prediction"])
