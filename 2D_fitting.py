04c8aeb2a5828216cabca77e907012dc53dcb69f
Tom Tyler <tom-tyler@outlook.com>
2022-10-13 21:38:11 +0100

    plots

--- 2D_fitting.py
+++ 2D_fitting.py
@@ -5,12 +5,16 @@ import matplotlib.pyplot as plt
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
 from matplotlib.colors import ListedColormap
-from scipy.interpolate import UnivariateSpline
+from scipy.stats.qmc import LatinHypercube
 
-grid_size=1000
+grid_size=2000
+training_data_size=20
+# testing_data_size=12
+
+sampler = LatinHypercube(d=1)
 x = np.linspace(start=-10, stop=10, num=grid_size)
 y = np.linspace(start=-10, stop=10, num=grid_size)
-# print(np.dstack((x,y)))
+
 X,Y = np.meshgrid(x,y) # creates two matrices which vary across in x and y
 X_vector = X.ravel() #vector of "all" x coordinates from meshgrid
 Y_vector = Y.ravel() #vector of "all" y coordinates from meshgrid
@@ -18,7 +22,8 @@ P = np.column_stack((X_vector,Y_vector))
 Q = P[:,0]**2 - P[:,1]**2
 Z = X**2 - Y**2
 
-training_indices = np.random.randint(low=len(Q),size=10)
+#training_indices = np.random.randint(low=len(Q),size=12)
+training_indices = (len(Q)*sampler.random(n=training_data_size)).astype(int).reshape(training_data_size)
 P_train, Q_train = P[training_indices], Q[training_indices]
 
 kernel = 1.0 * RBF(length_scale=(1.0,1.0))
@@ -26,8 +31,8 @@ gaussian_process = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=
 gaussian_process.fit(P_train, Q_train)
 print(gaussian_process.kernel_)
 
-testing_indices = np.random.randint(low=len(Q),size=20)
-P_train = P[testing_indices]
+# testing_indices = np.random.randint(low=len(Q),size=testing_data_size)
+# P_test = P[testing_indices]
 
 mean_prediction, std_prediction = gaussian_process.predict(P, return_std=True)
 upper_confidence_interval = mean_prediction + 1.96 * std_prediction
@@ -38,55 +43,25 @@ upper_confidence_interval_grid = upper_confidence_interval.reshape(grid_size,gri
 lower_confidence_interval_grid = lower_confidence_interval.reshape(grid_size,grid_size)
 
 contour_levels = np.linspace(-80,80,5)
-#if row upper_grid
 
 fig, ax = plt.subplots()
 actual_plot = ax.contour(X, Y, Z, colors='red',levels=contour_levels)
 predicted_plot = ax.contour(X, Y, mean_prediction_grid,colors='blue',levels=contour_levels)
-confidence_plot_upper = ax.contour(X, Y, upper_confidence_interval_grid,levels=contour_levels,colors='green')
-confidence_plot_lower = ax.contour(X, Y, lower_confidence_interval_grid,levels=contour_levels,colors='green')
+# confidence_plot_upper = ax.contour(X, Y, upper_confidence_interval_grid,levels=contour_levels,colors='green')
+# confidence_plot_lower = ax.contour(X, Y, lower_confidence_interval_grid,levels=contour_levels,colors='green')
 
 for contour_level in contour_levels:
     confidence_array = (upper_confidence_interval_grid>=contour_level) & (lower_confidence_interval_grid<=contour_level)
-    ax.contourf(X,Y,confidence_array, levels=[0.5, 2], cmap=ListedColormap(['green']), alpha=0.3)
+    confidence_plot = ax.contourf(X,Y,confidence_array, levels=[0.5, 2], cmap=ListedColormap(['blue']), alpha=0.3)
 
 
-
-# for contour_level in range(len(contour_levels)):
-    
-#     contour_lines_upper = confidence_plot_upper.allsegs[contour_level]
-#     contour_lines_lower = confidence_plot_lower.allsegs[contour_level]
-#     #plot when between level and std
-    
-#     for contour_line in range(len(contour_lines_upper)):
-        
-#         upper_contour_x = contour_lines_upper[contour_line][:,1]
-
-        
-#         lower_contour_x = contour_lines_lower[contour_line][:,1]
-        
-        
-        
-
-# ax.scatter(P_train[:,0],P_train[:,1],marker='x')
-# (zzmax <= 1) & (zzmin >= 1)
+ax.scatter(P_train[:,0],P_train[:,1],marker='x')
 h1,_ = actual_plot.legend_elements()
 h2,_ = predicted_plot.legend_elements()
-ax.legend([h1[0], h2[0]], [r"$f(x,y) = x^{2}-y^{2}$", "Mean prediction"])
+h3,_ = confidence_plot.legend_elements()
+ax.legend([h1[0], h2[0], h3[0]], [r"$f(x,y) = x^{2}-y^{2}$", "Mean prediction",r"95% confidence interval"])
 ax.set_xlabel("$x$")
 ax.set_ylabel("$y$")
 ax.set_title("Gaussian process regression on multivariate function")
 ax.set_aspect('equal','box')
 plt.show()
-# actual_plot = plt.contour(X,Y,Z, label=r"$f(x,y) = x^{2}-y^{2}$", colors='red')
-
-# plt.plot(P, Q, label=r"$f(x,y) = x^{2}-y^{2}$", linestyle="dotted")
-# plt.scatter(P_train, Q_train, label=r"$Samples$", marker="x")
-# predicted_plot = plt.contour(X,Y, mean_prediction_grid, label="Mean prediction",colors='blue')
-# plt.fill_between(
-#     x=X.ravel(),
-#     y1=upper_confidence_interval,   # 95% of area is within 1.96x standard deviation of the mean
-#     y2=lower_confidence_interval,
-#     alpha=0.5,                                       #transparency
-#     label=r"95% confidence interval"
-# )
