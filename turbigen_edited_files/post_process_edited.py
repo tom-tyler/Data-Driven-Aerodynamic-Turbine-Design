6df65e687d113a279340774dde31a127710b79ef
Tom Tyler <tom-tyler@outlook.com>
2022-11-17 19:05:47 +0000

    edited post_process

--- turbigen_edited_files/post_process_edited.py
+++ turbigen_edited_files/post_process_edited.py
@@ -13,17 +13,27 @@ import numpy as np
 # g = grid.read_hdf5(output_hdf5)
 
 # Load grid file manually by typing in runid
-job_name = str(input('Enter name of job results (e.g. data-A results) : '))
 datapoint_runid = str(input('Enter run id (e.g. 002803602529): '))
 
-datapoint_directory = os.path.join(job_name, datapoint_runid)
-datapoint_hdf5 = os.path.join(os.path.join(datapoint_directory, datapoint_runid), 'output_avg.hdf5')
+for root, dirs, files in os.walk('Results'):
+
+    if datapoint_runid in dirs:
+        job_directory = root
+        print('Datapoint is in: ',job_directory)
+        datapoint_directory = os.path.join(job_directory, datapoint_runid)
+        break
+
+datapoint_hdf5 = os.path.join(datapoint_directory, os.path.join(datapoint_runid, 'output_avg.hdf5'))
+print('HDF5 path: ', datapoint_hdf5)
 
 if os.path.isfile(datapoint_hdf5):
     g = grid.read_hdf5(datapoint_hdf5)
+    plot_directory = os.path.join(datapoint_directory, 'Figures')
+    os.makedirs(plot_directory)
 else:
     print('incorrect input')
     quit()
+
 
 
 # Extract cut (r, rt) or (y, z) planes upstream and downstream of each row
@@ -43,7 +53,7 @@ hc = ax.contourf(
 )
 ax.axis("equal")
 plt.colorbar(hc)
-plt.savefig(os.path.join(datapoint_directory,"contour_phi.pdf"))
+plt.savefig(os.path.join(plot_directory,"contour_phi.pdf"))
 
 # Now extract cut around the blade surfaces
 vane, blade = g.cut_blade_surfs()
@@ -53,7 +63,7 @@ fig, ax = plt.subplots()
 for jplot in (0, 33, -1):
     ax.plot(blade.x[:, jplot], blade.rt[:, jplot])
 ax.axis("equal")
-plt.savefig(os.path.join(datapoint_directory,"geometry.pdf"))
+plt.savefig(os.path.join(plot_directory,"geometry.pdf"))
 
 # Cut at mid-height (x, rt) plane
 C = g.cut_span(0.5)
@@ -65,4 +75,6 @@ for c in C:
     hc = ax.contourf(c.x, c.rt, c.ro, lev_rho)
 ax.axis("equal")
 plt.colorbar(hc)
-plt.savefig(os.path.join(datapoint_directory,"contour_rho.pdf"))
+plt.savefig(os.path.join(plot_directory,"contour_rho.pdf"))
+
+print('Figures stored in: ', plot_directory)
