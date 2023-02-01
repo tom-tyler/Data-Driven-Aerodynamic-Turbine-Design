4ea1477f9ef7f1d665ba4bc155a4524d3914944c
Tom Tyler <tom-tyler@outlook.com>
2023-02-01 14:41:50 +0000

    sdgvs

--- integration_test.py
+++ integration_test.py
@@ -4,9 +4,15 @@ rovx = np.array([[1,200,300],
                  [7,6,8],
                  [5,6,9]])
 
+p0 = np.array([[1,200,300],
+                 [7,6,8],
+                 [5,6,9]])
+
 r = np.array([0.2,0.3,0.4])
 rt = np.array([2,5,8])
 
 integral = np.trapz(rovx,r,axis=0)
 
 print(integral)
+
+print(p0*rovx)
